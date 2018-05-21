88a62cd51dc7b75d28eba287dd8a0c02202b1a11
Sebastián Gurin <sebastigurin@gmail.com>
2018-05-20 22:42:43 -0300

    test

--- typescript-api-tests/spec/codeGenSpec.ts
+++ typescript-api-tests/spec/codeGenSpec.ts
@@ -1,6 +1,7 @@
 import * as shell from 'shelljs'
-import Project from 'ts-simple-ast'
+import Project, {SourceFile, Node, Diagnostic} from 'ts-simple-ast'
 import * as ts from 'typescript'
+
 describe('method delegate interface', () => {
   const projectPath = `assets/sampleProject1_1_copy`;
   it(`interfaces`, () => {
@@ -9,49 +10,40 @@ describe('method delegate interface', () => {
     const project = new Project({
       tsConfigFilePath: `${projectPath}/tsconfig.json`
     });
-    // project.saveSync()
-    // project.emit()
-    // expect(project.getDiagnostics().length).toBe(0)
     const sourceFile = project.getSourceFiles().find(sf => sf.getFilePath().includes(`src/index.ts`));
     const fn = sourceFile.getFunction('main');
-    // const d = sourceFile.getDiagnostics()
     const diags = getDiagnosticsInCurrentLocation(project, sourceFile, 61);
     if (!diags || !diags.length) {
       return fail();
     }
-    // console.log(diags, diags.map(d=>d.getMessageText()))// }
     const diag = diags[0];
-    const child = sourceFile.getChildAtPos(61);
-    // const c2 = findChildContainingPosition(sourceFile, 61)
-    const childInPos = sourceFile.getDescendantAtPos(61);
-    const stmt = childInPos.getAncestors().find(anc => anc.getKind() === ts.SyntaxKind.ExpressionStatement);
-    const fixes = codeFixes.filter(fix => fix.predicate(diag, childInPos));
-    // choose one
+    const child = sourceFile.getDescendantAtPos(61);
+    // const stmt = child.getAncestors().find(anc => anc.getKind() === ts.SyntaxKind.ExpressionStatement);
+    const fixes = codeFixes.filter(fix => fix.predicate(diag, child));
     if (!fixes || !fixes.length) {
       return fail();
     }
-    debugger;
     fixes[0].apply(diag, child);
-    sourceFile.saveSync();
-    project.emit();
+    // sourceFile.saveSync();
+    // project.emit();
     project.saveSync();
     project.emit();
     console.log('Project saved');
   });
-  // doAssert(projectPath);
+  doAssert(projectPath);
 });
 const codeFixCreateVariable = {
   name: 'create variable',
   config: { variableType: 'const' },
-  predicate: (diag, child) => diag.getCode() === 2304 && // Cannot find name 'i'
+  predicate: (diag: Diagnostic, child:Node):boolean => diag.getCode() === 2304 && // Cannot find name 'i'
     child.getKind() === ts.SyntaxKind.Identifier &&
     child.getParent().getKind() === ts.SyntaxKind.BinaryExpression &&
     child.getParent().getParent().getKind() === ts.SyntaxKind.ExpressionStatement,
-  description: (diag, child) => `Create variable "${child.getText()}"`,
+  description: (diag: Diagnostic, child: Node) : string => `Create variable "${child.getText()}"`,
   // apply: (diag: Diagnostic, child: Node)=>{child.getSourceFile().insertText(child.getStart(false), 'const ')}
-  apply: (diag, child) => {
-    // child.getSourceFile().insertText(child.getStart(false), 'const '); 
-    child.getSourceFile().insertText(child.getStart(), '/* sebababab */');
+  apply: (diag: Diagnostic, child: Node) => {
+    child.getSourceFile().insertText(child.getStart(false), 'const '); 
+    // child.getSourceFile().insertText(child.getStart(), '/* sebababab */');
     // child.getSourceFile().applyTextChanges()
   }
 };
@@ -65,23 +57,21 @@ function getDiagnosticsInCurrentLocation(project, sourceFile, position) {
   // d.getSourceFile().getLineNumberFromPos() ===file.getLineNumberFromPos(position))
 }
 
-
-
-// export function findChildContainingPosition(sourceFile: SourceFile, position: number): Node | undefined {
-//   let found:Node
-//   function find(node:  Node, stop: ()=>void):void {
-//     if (position >= node.getStart() && position < node.getEnd()) {
-//       node.forEachChild(find)
-//       if(!found){
-//         found= node
-//         stop()
-//       }
-//     }
-//   }
-//   find(sourceFile, ()=>{})
-//   return found
-// }
-// function doAssert(projectPath) {
+export function findChildContainingPosition(sourceFile: SourceFile, position: number): Node | undefined {
+  let found:Node
+  function find(node:  Node, stop: ()=>void):void {
+    if (position >= node.getStart() && position < node.getEnd()) {
+      node.forEachChild(find)
+      if(!found){
+        found= node
+        stop()
+      }
+    }
+  }
+  find(sourceFile, ()=>{})
+  return found
+}
+function doAssert(projectPath) {
 //   it('interface should contain delegate method signatures', () => {
 //     // expect(cat(`${projectPath}/src/index.ts`).toString()).toContain('getCurrentSpeed(): number;')
 //   });
@@ -91,4 +81,4 @@ function getDiagnosticsInCurrentLocation(project, sourceFile, position) {
 //     // expect(project.getDiagnostics().length).toBe(0)
 //     // // console.log(project.getDiagnostics().map(d=>d.getMessageText()).join('\n'))
 //   });
-// }
+}
--- typescript-api-tests/spec/issue1.ts
+++ typescript-api-tests/spec/issue1.ts
@@ -1,17 +1,24 @@
 
 import Project from 'ts-simple-ast'
 import * as ts from 'typescript'
+import { rm } from 'shelljs';
 const project = new Project({
     compilerOptions: {
         target: ts.ScriptTarget.ES3
     }
 });
 const fileText = "enum MyEnum {\n}\n";
-const sourceFile = project.createSourceFile("/home/sg/git/typescript-plugins-of-mine/typescript-api-tests/assets/sampleProject1_1_copy/test11.ts", `function(){
+const filePath = "assets/sampleProject1_1_copy/test11.ts"
+rm('-rf', filePath)
+let sourceFile = project.createSourceFile(filePath, `export function f (){
   
 }
-var i = 1
+i = 1
 `);
-const i = sourceFile.getAncestors().find(a => a.getText() === 'i');
+
+sourceFile.saveSync()
+project.saveSync()
+project.emit()
+sourceFile = project.getSourceFile(filePath)
+const i = sourceFile.getDescendants().find(a => a.getText() === 'i');
 console.log(i.getStart(), i.getEnd());
-//# sourceMappingURL=issue1.js.map
