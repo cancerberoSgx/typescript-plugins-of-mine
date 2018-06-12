fe431902e99daa46a336471368919135769868e0
Sebastián Gurin <sebastigurin@gmail.com>
2018-06-12 19:56:56 -0300

    extractInterfaceNewOne

--- typescript-plugin-extract-interface-client-project1/src/evaltests/addReturnStatement.ts
+++ typescript-plugin-extract-interface-client-project1/src/evaltests/addReturnStatement.ts
@@ -4,8 +4,8 @@ function ghghg(t?: boolean[]): string[] | number {
 }
 const other = (a: string): number => {}
 //"code": "2355",	"message": "A function whose declared type is neither 'void' nor 'any' must return a value.",
-import {forgotttt} from './forgottenExport'
-forgotttt.method1()
+// import {forgotttt} from './forgottenExport'
+// forgotttt.method1()
 import { EvalContext } from 'typescript-plugin-ast-inspector';
 declare const c: EvalContext;
 
---
+++ typescript-plugin-extract-interface-client-project1/src/evaltests/extractInterfaceNewOne.ts
@@ -0,0 +1,128 @@
+class Fruit {
+  color: string
+  /** shouldn't be exported */
+  private creationDate: Date
+  foo() {
+    console.log('hello');
+    return Promise.resolve([['haha']])
+  }
+}
+
+const obj21 = {
+  color: 'hshs',
+  /** shouldn't be exported */
+  creationDate: new Date(),
+  foo: () => {
+    console.log('hello');
+    return Promise.resolve([new Date()])
+  },
+  bar(greet: string): Promise<string[]> { return null }
+}
+
+import { ClassDeclaration, InterfaceDeclarationStructure, MethodSignatureStructure, ObjectLiteralExpression, PropertySignatureStructure, Scope, TypeGuards, TypeParameterDeclarationStructure } from 'ts-simple-ast';
+import * as ts from 'typescript';
+import { EvalContext } from 'typescript-plugin-ast-inspector';
+declare const c: EvalContext;
+
+
+function fromClassDeclaration(source: ClassDeclaration) {
+  const interfaceName = `I${source.getName() || 'UnnamedInterface'}`
+  const structure: InterfaceDeclarationStructure = {
+    name: interfaceName,
+    properties: source.getProperties().filter(p => p.getScope() === Scope.Public).map(p => ({
+      name: p.getName(),
+      type: p.getTypeNode() ? p.getTypeNode().getText() : p.getType() ? p.getType().getText() : undefined,
+      questionToken: p.hasQuestionToken(),
+      isReadonly: p.isReadonly(),
+      initializer: p.getInitializer() ? p.getInitializer().getText() : undefined
+    } as PropertySignatureStructure)),
+    methods: source.getMethods().filter(p => p.getScope() === Scope.Public).map(m => ({
+      name: m.getName(),
+      returnType: m.getReturnTypeNode() ? m.getReturnTypeNode().getText() : m.getType() ? m.getType().getText() : undefined,
+      // typeParameters: m. //TODO
+      parameters: m.getParameters().map(p => ({
+        name: p.getName(),
+        type: p.getTypeNode() ? p.getTypeNode().getText() : p.getType() ? p.getType().getText() : undefined,
+        questionToken: p.hasQuestionToken(),
+        initializer: p.getInitializer() ? p.getInitializer().getText() : undefined
+      }))
+    } as MethodSignatureStructure)),
+    typeParameters: source.getTypeParameters() && source.getTypeParameters().map(tp => ({
+      name: tp.getName(),
+      constraint: tp.getConstraint() && tp.getConstraint().getText(),
+      default: tp.getDefault() && tp.getDefault().getText()
+    } as TypeParameterDeclarationStructure))
+  };
+  source.addImplements(interfaceName)
+  source.getSourceFile().insertInterface(source.getChildIndex(), structure)
+}
+
+function fromObjectLiteral(source: ObjectLiteralExpression) {
+  const interfaceName = `I${source.getFirstAncestorByKind(ts.SyntaxKind.VariableDeclaration) && source.getFirstAncestorByKind(ts.SyntaxKind.VariableDeclaration).getName() || 'UnnamedInterface'}`
+  let initializerProp, initializerMethod
+  const structure: InterfaceDeclarationStructure = {
+    name: interfaceName,
+    properties: source.getProperties().filter(TypeGuards.isPropertyAssignment).filter(p => (initializerProp = p.getInitializer()) && !TypeGuards.isSignaturedDeclaration(initializerProp)).map(p => ({
+      name: p.getName(),
+      type: p.getType() ? p.getType().getText() : 'undefined',
+    } as PropertySignatureStructure)),
+    methods: source.getProperties().filter(TypeGuards.isPropertyAssignment).filter(p => (initializerMethod = p.getInitializer()) && TypeGuards.isSignaturedDeclaration(initializerMethod)).map(m => ({
+      name: m.getName(),
+      returnType: initializerMethod.getReturnTypeNode() ? initializerMethod.getReturnTypeNode().getText() : initializerMethod.getReturnType() ? initializerMethod.getReturnType().getText() : undefined,
+      parameters: initializerMethod.getParameters().map(p => ({
+        name: p.getName(),
+        type: p.getTypeNode() ? p.getTypeNode().getText() : p.getType() ? p.getType().getText() : undefined,
+        questionToken: p.hasQuestionToken(),
+        initialize: initializerMethod ? initializerMethod.getText() : undefined
+      }))
+    } as MethodSignatureStructure))
+      .concat(source.getProperties().filter(TypeGuards.isMethodDeclaration).map(m => ({
+        name: m.getName(),
+        returnType: m.getReturnTypeNode() ? m.getReturnTypeNode().getText() : m.getReturnType() ? m.getReturnType().getText() : undefined,
+        parameters: m.getParameters().map(p => ({
+          name: p.getName(),
+          type: p.getTypeNode() ? p.getTypeNode().getText() : p.getType() ? p.getType().getText() : undefined,
+          questionToken: p.hasQuestionToken(),
+          initializer: p.getInitializer() ? p.getInitializer().getText() : undefined
+        }))
+      })))
+      .filter((p, i, arr) => arr.indexOf(p) === i), // de duplicating just in case
+  };
+  let parent = source.getParent()
+  if (TypeGuards.isVariableDeclaration(parent)) {
+    parent.setType(interfaceName)
+  }
+  source.getSourceFile().insertInterface(Math.max(0, source.getChildIndex() - 2), structure)
+}
+
+
+let print
+/**
+ * # Description
+ * 
+ * will extract interface from class declaration or object literal ancestor making it implement the new interface 
+ */
+function evaluateMe() {
+  print = c.print
+
+  const position = 234
+  const sourceFile = c.project.createSourceFile(`tmp/extractInterfaceNewOne${Date.now()}.ts`, c.node.getSourceFile().getText())
+
+  const node = sourceFile.getDescendantAtPos(position)
+
+  const source = node.getAncestors().find(a => TypeGuards.isClassDeclaration(a) || TypeGuards.isObjectLiteralExpression(a))
+  if (!source) {
+    print('not applying since cannot node.getAncestors().find(a => TypeGuards.isClassDeclaration(a) || TypeGuards.isObjectLiteralExpression(a))')
+    return
+  }
+  else if (TypeGuards.isClassDeclaration(source)) {
+    fromClassDeclaration(source)
+  } else if (TypeGuards.isObjectLiteralExpression(source)) {
+    fromObjectLiteral(source)
+  }
+
+  print(sourceFile.getText().substring(0, Math.min(sourceFile.getText().length, 800)))
+
+  sourceFile.deleteImmediatelySync()
+
+}
