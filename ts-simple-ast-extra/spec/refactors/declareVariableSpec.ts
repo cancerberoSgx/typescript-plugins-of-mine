6c2937de9f52ee33d28dbb0b58c016e30733ab41
Sebastián Gurín <sgurin@awalabs.com>
2019-04-06 04:38:57 -0300

    declareVariable

--- ts-simple-ast-extra/spec/refactors/declareVariableSpec.ts
+++ ts-simple-ast-extra/spec/refactors/declareVariableSpec.ts
@@ -1,16 +1,17 @@
-import { removeWhites } from 'misc-utils-of-mine-generic';
-import { Identifier, Project, TypeGuards } from 'ts-morph';
-import { declareVariable, declareVariables } from '../../src';
+import { removeWhites } from 'misc-utils-of-mine-generic'
+import { Identifier, Project, TypeGuards } from 'ts-morph'
+import { declareVariable, declareVariables } from '../../src'
 
 describe('declareVariable', () => {
   describe('declareVariable', () => {
     it('should declare a function if a call expression references one not defined. ', () => {
       const project = new Project()
-      const f = project.createSourceFile(
-        'test.ts',
-        `const a = nonDeclared(2,'s')`
+      const f = project.createSourceFile('test.ts', `const a = nonDeclared(2,'s')`)
+      declareVariable(
+        f.getDescendants().find(d => TypeGuards.isIdentifier(d) && d.getText() === 'nonDeclared')! as Identifier,
+        project.getTypeChecker(),
+        f.getPreEmitDiagnostics()
       )
-      declareVariable(f.getDescendants().find(d=>TypeGuards.isIdentifier(d) &&d.getText()==='nonDeclared' )! as Identifier, project.getTypeChecker(), f.getPreEmitDiagnostics())
       expect(removeWhites(f.getText())).toBe(
         removeWhites(
           `
@@ -24,11 +25,12 @@ describe('declareVariable', () => {
     })
     it('should declare a add let to a non declared variable assignation', () => {
       const project = new Project()
-      const f = project.createSourceFile(
-        'test.ts',
-        `a = [1,2]`
+      const f = project.createSourceFile('test.ts', `a = [1,2]`)
+      declareVariable(
+        f.getDescendants().find(d => TypeGuards.isIdentifier(d) && d.getText() === 'a')! as Identifier,
+        project.getTypeChecker(),
+        f.getPreEmitDiagnostics()
       )
-      declareVariable(f.getDescendants().find(d=>TypeGuards.isIdentifier(d) &&d.getText()==='a' )! as Identifier, project.getTypeChecker(), f.getPreEmitDiagnostics())
       expect(removeWhites(f.getText())).toBe(
         removeWhites(
           `
--- ts-simple-ast-extra/src/refactor/declareVariable.ts
+++ ts-simple-ast-extra/src/refactor/declareVariable.ts
@@ -1,4 +1,16 @@
-import { VariableDeclarationList, Node, TypeGuards, VariableStatementStructure, SyntaxKind, TypeChecker, Diagnostic, Identifier, Block, StatementedNode, Statement } from 'ts-morph'
+import {
+  VariableDeclarationList,
+  Node,
+  TypeGuards,
+  VariableStatementStructure,
+  SyntaxKind,
+  TypeChecker,
+  Diagnostic,
+  Identifier,
+  Block,
+  StatementedNode,
+  Statement
+} from 'ts-morph'
 /**
  * Replace given node's VariableDeclarationList descendants with individual variable declaration statements one for each variable.
  * It will add types if variables in variable declaration list don't declare them
@@ -7,7 +19,9 @@ export function declareVariables(node: Node, tc: TypeChecker, diagnostics: Diagn
   let c: Identifier | undefined
   while (
     (c = node.getFirstDescendant(
-      desc => TypeGuards.isIdentifier(desc)&& !!diagnostics.find(d => (d.getCode() === 2304 || d.getCode() === 2552) && d.getStart() ===desc.getStart())
+      desc =>
+        TypeGuards.isIdentifier(desc) &&
+        !!diagnostics.find(d => (d.getCode() === 2304 || d.getCode() === 2552) && d.getStart() === desc.getStart())
     ) as Identifier)
   ) {
     declareVariable(c, tc, diagnostics)
@@ -20,16 +34,24 @@ export function declareVariables(node: Node, tc: TypeChecker, diagnostics: Diagn
  * It will add types if variables in variable declaration list don't declare them
  */
 export function declareVariable(node: Identifier, tc: TypeChecker, diagnostics: Diagnostic[]) {
-  if(!diagnostics.find(d => (d.getCode() === 2304 || d.getCode() === 2552) && d.getStart() === node.getStart())){
+  if (!diagnostics.find(d => (d.getCode() === 2304 || d.getCode() === 2552) && d.getStart() === node.getStart())) {
     throw 'cannot declare variable if there is no 2304 or 2552 error'
   }
   const parent = node.getParent()
   const container = parent.getFirstAncestorByKind(SyntaxKind.Block) || node.getSourceFile()
-  const statementAncestor = parent.getAncestors().find(a => a.getParent() === container) as Statement// || node.getSourceFile()
-  if (TypeGuards.isIdentifier(node) && TypeGuards.isCallExpression(parent) && TypeGuards.isIdentifier(parent.getExpression()) && parent.getExpression().getText() === node.getText()) {
+  const statementAncestor = parent.getAncestors().find(a => a.getParent() === container) as Statement // || node.getSourceFile()
+  if (
+    TypeGuards.isIdentifier(node) &&
+    TypeGuards.isCallExpression(parent) &&
+    TypeGuards.isIdentifier(parent.getExpression()) &&
+    parent.getExpression().getText() === node.getText()
+  ) {
     const functionName = node.getText()
     const functionArguments = parent.getArguments().map((a, index) => {
-      const type = tc.getTypeAtLocation(a).getBaseTypeOfLiteralType().getText()
+      const type = tc
+        .getTypeAtLocation(a)
+        .getBaseTypeOfLiteralType()
+        .getText()
       return `arg${index}: ${type}`
     })
     const t = tc.getContextualType(node.getFirstAncestorByKindOrThrow(SyntaxKind.CallExpression))
@@ -41,25 +63,27 @@ function ${functionName}(${functionArguments.join(', ')}): ${returnType} {
 `
     if (!TypeGuards.isPropertyAccessExpression(parent.getChildAtIndex(0))) {
       // it's function call and the function is not a member i.e bar()
-      (statementAncestor.getParent()! as Block).insertStatements(statementAncestor.getChildIndex(), functionText)
-    }
-    else {
+      ;(statementAncestor.getParent()! as Block).insertStatements(statementAncestor.getChildIndex(), functionText)
+    } else {
       // it's a function call and the function is a member, i.e : foo.bar() - this is tackled by another fix: declareMember
       // TODO
     }
-  }
-  else {
+  } else {
     // its non function variable
-    if (TypeGuards.isBinaryExpression(node.getParentOrThrow()) && TypeGuards.isStatement(node.getParentOrThrow().getParentOrThrow())) {
+    if (
+      TypeGuards.isBinaryExpression(node.getParentOrThrow()) &&
+      TypeGuards.isStatement(node.getParentOrThrow().getParentOrThrow())
+    ) {
       // is an expression like a=1 we only preppend 'let '
-      (statementAncestor.getParent() as Block).insertStatements(statementAncestor.getChildIndex(), `let ${node.getParent()!.getText()}`)
-      statementAncestor.remove();
-    }
-    else {
+      ;(statementAncestor.getParent() as Block).insertStatements(
+        statementAncestor.getChildIndex(),
+        `let ${node.getParent()!.getText()}`
+      )
+      statementAncestor.remove()
+    } else {
       // other wise we create a new statement 'let a' at the top
       // TODO: test
-      (statementAncestor.getParent()! as Block).insertStatements(0, `let ${(node as Identifier).getText()};\n`)
+      ;(statementAncestor.getParent()! as Block).insertStatements(0, `let ${(node as Identifier).getText()};\n`)
     }
   }
 }
-
